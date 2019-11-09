e2480de5700baca0a9d0f653c22c387940965619
Jaeyoon Kim <jaeykim@umich.edu>
2019-11-09 17:06:55 -0500

    Added syntax error in test

--- lib/test_save_list.cpp
+++ lib/test_save_list.cpp
@@ -9,27 +9,27 @@ using namespace fb;
 int main() {
    try {
    SavedLists<SizeT> file("test_file");
-   vector<SizeT> test_adj_list_1 = {1, 11};
-   vector<SizeT> test_adj_list_2 = {0};
-   vector<SizeT> test_adj_list_3 = {7, 10, 1234};
+   vector<SizeT> test_list_1 = {1, 11};
+   vector<SizeT> test_list_2 = {0};
+   vector<SizeT> test_list_3 = {7, 10, 1234};
 
-   SizeT offset1 = file.add_list( test_adj_list_1 );
-   SizeT offset2 = file.add_list(test_adj_list_2);
-   SizeT offset3 = file.add_list(test_adj_list_3);
+   SizeT offset1 = file.add_list( test_list_1 );
+   SizeT offset2 = file.add_list( test_list_2);
+   SizeT offset3 = file.add_list( test_list_3);
    
    SizeT* val = file.get_list(offset1);
    for (int i = 0; i < test_list_1.size(); ++i) {
-      assert(test_adj_list_1[i] == val[i]);
+      assert(test_list_1[i] == val[i]);
    }
 
-   val = file.get_adj_list(offset2);
+   val = file.get_list(offset2);
    for (int i = 0; i < test_list_2.size(); ++i) {
-      assert(test_adj_list_2[i] == val[i]);
+      assert(test_list_2[i] == val[i]);
    }
 
-   val = file.get_adj_list(offset3);
+   val = file.get_list(offset3);
    for (int i = 0; i < test_list_3.size(); ++i) {
-      assert(test_adj_list_3[i] == val[i]);
+      assert(test_list_3[i] == val[i]);
    }
 
    } catch ( Exception& e) {

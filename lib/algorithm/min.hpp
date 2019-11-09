fca631dedc83d7d941ea6bc8f486fdba02cb5cc3
Aniruddh Agarwal <me@anrddh.me>
2019-11-09 16:10:28 -0500

    Use correct default constructor in min/min_element impl

--- lib/algorithm/min.hpp
+++ lib/algorithm/min.hpp
@@ -9,15 +9,12 @@
 namespace fb {
 
     template <typename T, typename Compare = Less<T>>
-    constexpr const T & min(const T &a,
-                            const T &b,
-                            Compare comp = Less<T>()) {
+    constexpr const T & min(const T &a, const T &b, Compare comp = Compare()) {
         return comp(a,b) ? a : b;
     }
 
     template <typename T, typename Compare = Less<T>>
-    constexpr T min(std::initializer_list<T> ilist,
-                    Compare comp = Less<T>()) {
+    constexpr T min(std::initializer_list<T> ilist, Compare comp = Compare()) {
         return *min_element(ilist.begin(), ilist.end(), comp);
     }
 
--- lib/algorithm/min_element.hpp
+++ lib/algorithm/min_element.hpp
@@ -4,23 +4,10 @@
 #include "../iterator.hpp"
 
 namespace fb {
-    //template <typename It>
-    //constexpr It min_element(It begin, It end) {
-    //    if (begin == end)
-    //        return end;
-
-    //    auto min = begin;
-
-    //    for (; begin != end; ++begin)
-    //        if (*begin < *min)
-    //            min = begin;
-
-    //    return min;
-    //}
 
     template <typename It,
               typename Compare = Less<typename IteratorTraits<It>::ValueType>>
-    constexpr It min_element(It begin, It end, Compare comp) {
+    constexpr It min_element(It begin, It end, Compare comp = Compare()) {
         if (begin == end)
             return end;
 

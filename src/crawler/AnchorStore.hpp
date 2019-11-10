43eaf9839c51d13ba56ded913f6ca02e2500efd9
Aniruddh Agarwal <me@anrddh.me>
2019-11-10 17:17:18 -0500

    Make AnchorStore and UrlStore singletons

--- src/crawler/AnchorStore.hpp
+++ src/crawler/AnchorStore.hpp
@@ -19,19 +19,25 @@ struct ChunkImpl {
 
 using Chunk = ChunkImpl<56, uint64_t>;
 
+// Singleton
 class AnchorStore {
 public:
-    AnchorStore(fb::StringView filename) : anchors(filename) {
-        addStr("", {0, 0});
+    static void init(fb::StringView filename) {
+        delete ptr;
+        ptr = new AnchorStore(filename);
     }
 
-    //Recieves a new anchor text and a current begin and end offset for the
-    //URL's anchor text. Adds the anchor text and returns the new begin and end.
-    //Note: the begin offset should only be different if this is the first time
-    //seeing anchor text for this url.
-    fb::Pair<fb::SizeT, fb::SizeT>
-    addStr(fb::StringView anchorText,
-           fb::Pair<fb::SizeT, fb::SizeT> offsets) {
+    AnchorStore & getStore() {
+        return *ptr;
+    }
+
+    // Recieves a new anchor text and a current begin and end offset
+    // for the URL's anchor text. Adds the anchor text and returns the
+    // new begin and end.  Note: the begin offset should only be
+    // different if this is the first time seeing anchor text for this
+    // url.
+    fb::Pair<fb::SizeT, fb::SizeT> addStr(fb::StringView anchorText,
+                                          fb::Pair<fb::SizeT,fb::SizeT> offsets) {
         // Special case: if the anchor text is empty we dont change
         // anything
         if (anchorText.empty())
@@ -61,5 +67,10 @@ public:
     //Do we need this? not for now at least probably TODO
     //char *get_str(SizeT offset) {}
 private:
+    AnchorStore(fb::StringView filename) : anchors(filename) {
+        addStr("", {0, 0});
+    }
+
+    static AnchorStore *ptr;
     DiskVec<Chunk> anchors;
 };

8c3e8723980e8ce59eeadd4d188fb1fd83633772
Aniruddh Agarwal <me@anrddh.me>
2019-11-09 23:13:52 -0500

    Implement file descriptor

---
+++ lib/file_descriptor.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "utility.hpp"
+
+#include <exception>
+
+#include <unistd.h>
+
+namespace fb {
+
+    struct FileDesc {
+        struct ConstructionError : std::exception {
+            const char *msg = "";
+
+            ConstructionError(const char *msg_) : msg(msg_) {}
+
+            virtual const char * what() const noexcept {
+                return msg;
+            }
+        };
+
+        int fd = -1;
+
+        FileDesc() = default;
+
+        FileDesc(int fd_in) : fd(fd_in) {
+            if (fd_in < -1)
+                throw ConstructionError("Invalid file descriptor.");
+        }
+
+        FileDesc(FileDesc &&rhs) noexcept : fd(rhs.fd) {
+            rhs.fd = -1;
+        }
+
+        FileDesc & operator=(FileDesc &&rhs) noexcept {
+            fb::swap(fd, rhs.fd);
+        }
+
+        FileDesc(FileDesc &rhs) {
+            auto new_desc = dup(fd);
+            if (new_desc == -1)
+                throw ConstructionError("Failed to dup.");
+            rhs.fd = new_desc;
+        }
+
+        FileDesc & operator=(FileDesc rhs) {
+            fb::swap(fd, rhs.fd);
+        }
+
+        ~FileDesc() {
+            close(fd);
+        }
+
+        constexpr operator int() noexcept {
+            return fd;
+        }
+
+    };
+
+};

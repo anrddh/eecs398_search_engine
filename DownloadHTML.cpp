7e2da1bf3a413386cf6770e4af807ded9f67e5bd
Jin Soo Ihm <jinihm@umich.edu>
2019-11-17 16:25:38 -0500

    Added status code

--- DownloadHTML.cpp
+++ DownloadHTML.cpp
@@ -20,87 +20,90 @@ fb::String readString( fb::String filename )
 {
    std::ifstream ifs(filename.data());
    std::string content( (std::istreambuf_iterator<char>(ifs)),
-         (std::istreambuf_iterator<char>()    ) );
+		 (std::istreambuf_iterator<char>()    ) );
    ifs.close();
    return fb::String(content.c_str());
 }
 
 void saveString( const fb::String &content, fb::String filename )
 {
-    std::ofstream outfile;
+	std::ofstream outfile;
    outfile.open( filename.data() );
    outfile << content;
    outfile.close();
 }
 
 int main( int argc, char *argv[ ] ) 
-    {
-	fb::Queue<fb::String> urls;
-	fb::UnorderedSet<fb::String> visited_urls;
-	urls.push(fb::String(argv[1]));
+	{
+	// fb::Queue<fb::String> urls;
+	// fb::UnorderedSet<fb::String> visited_urls;
+	// urls.push(fb::String(argv[1]));
 
-	while(!urls.empty())
-		{
-		fb::String url = urls.front();
-		urls.pop();
-		if(visited_urls.find(url) != visited_urls.end())
-				continue;
+	// while(!urls.empty())
+	// 	{
+	// 	fb::String url = urls.front();
+	// 	urls.pop();
+	// 	if(visited_urls.find(url) != visited_urls.end())
+	// 			continue;
 
-		std::cout << "Parsed URL is: " << url << std::endl;
-		if(url.size() < 7)
-				continue;
+	// 	std::cout << "Parsed URL is: " << url << std::endl;
+	// 	if(url.size() < 7)
+	// 			continue;
 
-		std::cout << "\t Downloading " << url << std::endl;
-        fb::String content;
-		fb::String filename = "htmls_new/";
-		for(int i = 0; i < url.size(); ++i)
-    		{
-			if(isalnum(url[i]))
-				filename += url[i];
-    		}
-		filename += ".html";
-		try
-			{
-			PrintHtml( url, content );
-			}
-		catch (...)
-			{
-			std::cerr << "\t Failed at link" << std::endl;
-			continue;
-			}
+	// 	std::cout << "\t Downloading " << url << std::endl;
+ //        fb::String content;
+	// 	fb::String filename = "htmls_new/";
+	// 	for(int i = 0; i < url.size(); ++i)
+ //    		{
+	// 		if(isalnum(url[i]))
+	// 			filename += url[i];
+ //    		}
+	// 	filename += ".html";
+	// 	try
+	// 		{
+	// 		PrintHtml( url, content );
+	// 		}
+	// 	catch (...)
+	// 		{
+	// 		std::cerr << "\t Failed at link" << std::endl;
+	// 		continue;
+	// 		}
 
-		visited_urls.insert(url);
+	// 	visited_urls.insert(url);
 
-		writeVisitedUrls(url);
+	// 	writeVisitedUrls(url);
 
-        saveString( content, filename );
+ //        saveString( content, filename );
 
-		auto pa = ParsedUrl(url);
-		fb::String domain = pa.Service + "://" + pa.Host;
+	// 	auto pa = ParsedUrl(url);
+	// 	fb::String domain = pa.Service + "://" + pa.Host;
 
-		fb::Parser parser( content,  domain );
-		parser.parse();
+	// 	fb::Parser parser( content,  domain );
+	// 	parser.parse();
 
-        // std::cout << "Found urls" << std::endl;
-        // std::cout << "domain: " << domain << std::endl;
-		for ( auto i = parser.urlAnchorText.begin(); i != parser.urlAnchorText.end(); ++i )
-		{
-            // std::cout << i.key() << std::endl;
-			urls.push(i.key());
-		}
+ //        // std::cout << "Found urls" << std::endl;
+ //        // std::cout << "domain: " << domain << std::endl;
+	// 	for ( auto i = parser.urlAnchorText.begin(); i != parser.urlAnchorText.end(); ++i )
+	// 	{
+ //            // std::cout << i.key() << std::endl;
+	// 		urls.push(i.key());
+	// 	}
 
-        saveString( parser.getParsedResult(), filename + "_tagless");
-    	}
+ //        saveString( parser.getParsedResult(), filename + "_tagless");
+ //    	}
 
-		// fb::String url = argv[ 1 ];
-  //     fb::String downloadedContent;
-		// bool a = PrintHtml( url, downloadedContent );
-  //     std::cout << "print html" << std::endl;
-  //     std::cout << a << std::endl;
-  //     std::cout << downloadedContent << std::endl;
+		fb::String url = argv[ 1 ];
+      fb::String downloadedContent;
+      auto ht = HTTPDownloader();
+      auto a = ht.PrintHtml( url );
+      std::cout << "print html" << std::endl;
+      // std::cout << a << std::endl;
+      std::cout << a.downloadedContent << std::endl;
+      std::cout << "response " << a.response << std::endl;
 
-  //     bool b = PrintPlainTxt( url, downloadedContent );
-  //     std::cout << "print plain txt" << std::endl;
-  //     std::cout << b << std::endl;
-  //     std::cout << downloadedContent << std::endl;
-    }
+      // auto htt = HTTPDownloader();
+      // auto b = htt.PrintPlainTxt( url );
+      // std::cout << "print plain txt" << std::endl;
+      // // std::cout << b << std::endl;
+      // std::cout << b.downloadedContent << std::endl;
+	}

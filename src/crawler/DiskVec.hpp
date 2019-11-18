899dd5841bccff0dd7c477d42b46d498424e7cdb
Aniruddh Agarwal <me@anrddh.me>
2019-11-18 03:01:27 -0500

    Fix bug in DiskVec

--- src/crawler/DiskVec.hpp
+++ src/crawler/DiskVec.hpp
@@ -29,7 +29,7 @@ constexpr fb::SizeT MAXFILESIZE = 0x1000000000; // 128 GiB
 template <typename T>
 class DiskVec {
 public:
-    DiskVec(fb::StringView fname, bool isInitd = false)  {
+    DiskVec(fb::StringView fname, bool init = false)  {
         fb::FileDesc fd = open(fname.data(),
                                O_RDWR | O_CREAT,
                                S_IRUSR | S_IWUSR | S_IROTH | S_IWOTH);
@@ -43,10 +43,10 @@ public:
         if (ptr == (void *) -1)
             throw fb::Exception("SavedObj: Failed to mmap.");
 
-        if (isInitd)
-            cursor = static_cast<std::atomic<fb::SizeT> *>(ptr);
-        else
+        if (init)
             cursor = new (ptr) std::atomic<fb::SizeT>(0);
+        else
+            cursor = static_cast<std::atomic<fb::SizeT> *>(ptr);
         filePtr = reinterpret_cast<T *>(cursor + 1);
     }
 

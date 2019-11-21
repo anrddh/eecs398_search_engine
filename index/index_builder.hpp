2bdca3b9c84ea0f3ca187a3a1d22077bdb387fb3
Sharon Ye <sharonye@umich.edu>
2019-11-21 17:24:18 -0500

    changed code to read in PageStore file

--- index/index_builder.hpp
+++ index/index_builder.hpp
@@ -40,29 +40,65 @@ public:
       
       tokenCount = 1;
    }
-   //takes in vector of IndexWords, maps words to indices in vector/flags
-   bool addDocument(unsigned int docId, fb::Vector<IndexWord> &words) {
-      wordPositionsLock.lock();
-      for(auto word : words)
-      {
-         AbsoluteWordInfo info = {tokenCount, word.type_flags}; 
-         wordPositions[word.word].pushBack(info);
-         ++wordDocCounts[word.word];
-         ++tokenCount;
+   
+   void build_chunk(size_t* start_of_file){
+      // move past first 16 bytes
+      // the page headers end where the first page starts
+      size_t* current_doc_offset = start_of_file + 2;
+      size_t* current_des_offset = start_of_file + 3;
+      size_t* current_doc_id = start_of_file + 4;
+      size_t* end_page_headers = start_of_file + *start_of_file;
+      while(current != end_page_headers){
+         build_single_doc(start_of_file + *current_doc, start_of_file + *current_des, *current_doc_id);
+         current_doc_offset += 3;
+         current_des_offset += 3;
+         current_doc_id += 3;
       }
-
-      // what is emplace_back? emplace back forwards the arguments you pass it to the constructor of type T. It constructs an object of type T in place which is why it is useufl
-      documents.emplaceBack(tokenCount, docId);
+      flushToDisk();
+   }
+private:
+   // reads a single space terminated word
+   // returns pointer to the beginning of the next word
+   // increments tokenCount
+   char* read_word(char* &word_begin, fb::String &word){
+      word = "";
+      while(*word_begin != ' ' && *word_begin != '\0'){
+         word = word + *word_begin;
+         ++word_begin;
+      }
       ++tokenCount;
-
-      constexpr int MAX_TOKEN_COUNT = 2 << MAX_BITS_PER_CHUNK;
-      if(tokenCount > MAX_TOKEN_COUNT) {
-         flushToDisk();
+      // check to see if we are at the end of a document
+      if(*word_begin == '\0'){
+         return word_begin;
       }
-      wordPositionsLock.unlock();
+      // otherwise move past the space and return the start
+      // of the next word
+      ++word_begin;
+      return word_begin;
    }
 
-private:
+   void build_single_doc(char* doc_start, uint8_t* des_start, uint64_t docId){
+      fb::UnorderedSet<fb::String word> unique_words;
+      fb::String word;
+      uint8_t word_info;
+      char* current_word = doc_start;
+      uint8_t* current_des = des_start;
+      while(*current_word != '\0'){
+         current_word = read_word(current_word, word);
+         unique_words.add(word);
+         word_info = *current_des;
+         AbsoluteWordInfo absWord = {tokenCount, word_info};
+         wordPositions[word].pushBack(absWord);
+      }
+      for(thing : unique_words){
+         ++wordDocCounts[thing];
+      }
+      // increment for EOD
+      ++tokenCount;
+      DocIdInfo doc_info = {tokenCount, docId};
+      documents.pushBack(doc_info);
+   }
+   
    void flushToDisk() 
       {
       WriteToDiskInput * input = new WriteToDiskInput;
@@ -101,11 +137,15 @@ private:
 
    // the actual map that stores the positions of the words, is a unique pointer so that we can pass this ownership to a thread.
    fb::UnorderedMap<fb::String, fb::Vector<AbsoluteWordInfo>> wordPositions;
+   // this is the number of documents a word appears in
    fb::UnorderedMap<fb::String, unsigned int> wordDocCounts;
   	fb::Mutex wordPositionsLock;
    fb::Vector<DocIdInfo> documents;
-    
+
    // each chunk keeps track of its own word count
-   int tokenCount;
+   unsigned long tokenCount;
   	
 };
+
+
+

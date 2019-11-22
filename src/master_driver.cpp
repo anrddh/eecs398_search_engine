5f321dd4fde2421e37d0e62f64fcfa6c02f7fc39
Jaeyoon Kim <jaeykim@umich.edu>
2019-11-21 20:55:41 -0500

    Revereted master_driver

--- src/master_driver.cpp
+++ src/master_driver.cpp
@@ -97,54 +97,12 @@ int main(int argc, char **argv) try {
     //Frontier::init("/tmp/frontier-bin.", false);
     perror("perror 1"); 
 
-    /*
     FileDesc sock = parseArguments( argc, argv );
     perror("perror 2"); 
     int sock_val = sock; // TODO delete
     std::cout << "main got socket" << sock << std::endl;
-    */
-    
-    // ===============================================================
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                                                  &opt, sizeof(opt)))
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address,
-                                 sizeof(address))<0)
-    {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 3) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    // ===============================================================
-    handle_socket(&server_fd);
+    handle_socket(&sock_val);
     //Thread socket_handler(handle_socket, &sock_val);
-
-    while (true);
 
     while (true) {
         auto &urlStore = UrlStore::getStore();
--- test/test_master_tcp.cpp
+++ test/test_master_tcp.cpp
@@ -32,12 +32,24 @@ int main(int argc, char const *argv[])
     } 
        
     // Forcefully attaching socket to the port 8000 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
-                                                  &opt, sizeof(opt))) 
-    { 
-        perror("setsockopt"); 
-        exit(EXIT_FAILURE); 
-    } 
+    
+    if (setsockopt(server_fd,
+                   SOL_SOCKET,
+                   SO_REUSEPORT,
+                   &opt, sizeof(opt)))
+    {
+        perror("setsockopt 1");
+        exit(EXIT_FAILURE);
+    }
+    if (setsockopt(server_fd,
+                   SOL_SOCKET,
+                   SO_REUSEADDR,
+                   &opt, sizeof(opt)))
+    {
+        perror("setsockopt 2");
+        exit(EXIT_FAILURE);
+
+    }
     address.sin_family = AF_INET; 
     address.sin_addr.s_addr = INADDR_ANY; 
     address.sin_port = htons( PORT ); 

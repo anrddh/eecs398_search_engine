bc94e3807a07935f3072bcbf978ba378b2908bc9
Jaeyoon <jaeykim@umich.edu>
2019-12-07 21:43:45 -0500

    modified tcp parameters

--- include/disk/frontier.hpp
+++ include/disk/frontier.hpp
@@ -16,9 +16,9 @@
 
 constexpr fb::SizeT NumFrontierBins = 13;
 // we will only randomly choose from first SEARCH_RESTRICTION number of elements
-constexpr fb::SizeT SEARCH_RESTRICTION= 131072;
+constexpr fb::SizeT SEARCH_RESTRICTION= 8388608;
 
-constexpr fb::SizeT NUM_TRY = 4000;
+constexpr fb::SizeT NUM_TRY = 100000;
 constexpr fb::SizeT NUM_SAMPLE = 3;
 
 // The minimum number of bits required to

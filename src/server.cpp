9b33fbb88ac9ddbbdddb568d94150c146afa2b5f
Jin Soo Ihm <jinihm@umich.edu>
2019-12-10 22:32:54 -0500

    hard coded some results

--- src/server.cpp
+++ src/server.cpp
@@ -23,7 +23,14 @@ HtmlPage kthResults() {
 
     fb::String counterString = fb::toString(resultCounter);
 
-    if( resultOptions["query"] == "aniruddh" )
+    if( resultOptions["query"] == "fantastic+bugs" )
+    {
+        page.setValue("title0", "Fanatstic Bugs Presentation");
+        page.setValue("url0", "https://docs.google.com/presentation/d/1VGzVglZy6j6fE8RD95SxyeLF8HCY3NdpRwY29NMRGEw/edit#slide=id.p");
+        page.setValue("snippet0", "");
+    }
+
+    else if( resultOptions["query"] == "aniruddh" )
     {
         page.setValue("title0", "Aniruddh Agarwal - MATH 593 (Graduate Algebra I) Grader...");
         page.setValue("url0", "https://www.linkedin.com/in/agarwalaniruddh");
@@ -47,13 +54,23 @@ HtmlPage kthResults() {
     }
     else
     {
-        int numResults = 10;
-        for(int i = 0; i < numResults; ++i) {
-            fb::String iString = fb::toString(i);
-            page.setValue("title" + iString, counterString +  iString + "th Result for " + resultOptions["query"]);
-            page.setValue("url" + iString, counterString + iString + "th Url");
-            page.setValue("snippet" + iString, counterString + iString + "th Snippet");
-        }
+        // int numResults = 10;
+        // for(int i = 0; i < numResults; ++i) {
+        //     fb::String iString = fb::toString(i);
+        //     page.setValue("title" + iString, counterString +  iString + "th Result for " + resultOptions["query"]);
+        //     page.setValue("url" + iString, counterString + iString + "th Url");
+        //     page.setValue("snippet" + iString, counterString + iString + "th Snippet");
+        // }
+        page.setValue("title0", "Did you mean Nicole Hamilton?");
+        page.setValue("url0" , "https://hamiltonlabs.com/");
+
+        page.setValue("title1", "Hamilton C shell");
+        page.setValue("url1", "https://en.wikipedia.org/wiki/Hamilton_C_shell");
+        page.setValue("snippet1", "Hamilton C shell is a clone of the Unix C shell and utilities[1][2] for Microsoft Windows created by Nicole Hamilton[3] at Hamilton Laboratories as a completely original work, not based on any prior code. It was first released on OS/2 on December 12, 1988[4][5][6][7][8][9] and on Windows NT in July 1992.[10][11][12] The OS/2 version was discontinued in 2003 but the Windows version continues to be actively supported.");
+
+        page.setValue("title2", "EECS 280: Programming and Intro Data Structures");
+        page.setValue("url2", "https://eecs280staff.github.io/eecs280.org/");
+        page.setValue("snippet2", "Computer science fundamentals, with programming in C++. Build an image processing program, a game of Euchre, a web backend, and a machine learning algorithm.");
     }
 
     return page;

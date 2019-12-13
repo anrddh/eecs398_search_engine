25e991f06f372dc4aaa8d189d6dbe8ea5ff12e24
grant-rez <grantreszczyk@gmail.com>
2019-12-13 06:25:37 +0000

    merge_index now works

--- scripts/check_documents.sh
+++ scripts/check_documents.sh
@@ -4,16 +4,10 @@ PAGESTORE_FILE=$1
 
 for i in $(seq 0 10000); do
     ALL_FOUND=true
-    if [[ ! $(./print_page_store $PAGESTORE_FILE $i | grep  " $2\$" ) ]]; then 
-        ALL_FOUND=false 
-    fi
-    if [[ ! $(./print_page_store $PAGESTORE_FILE $i | grep " $3\$" ) ]]; then 
-        ALL_FOUND=false 
-    fi
-    if [[ ! $(./print_page_store $PAGESTORE_FILE $i | grep " $4\$" ) ]]; then 
+    if [[ ! $(./print_page_store $PAGESTORE_FILE $i | grep " $2\$" ) ]]; then 
         ALL_FOUND=false 
     fi
     if [[ "$ALL_FOUND" == "true" ]]; then
-        echo "$i"
+        echo "$(( i+4119 )) "
     fi
 done

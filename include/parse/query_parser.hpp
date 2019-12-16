d77d5c69a39e668a659be051e4f661b2f35e8478
Aniruddh Agarwal <me@anrddh.me>
2019-12-16 16:08:04 -0500

    kill me plez

--- include/parse/query_parser.hpp
+++ include/parse/query_parser.hpp
@@ -37,6 +37,7 @@ public:
                     if (c == '\'') {
                         gotQuote = true;
                         return true;
+                    }
 
                     return false;
                 });

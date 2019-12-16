4e43a36f6265ba617492b5b78f656e61a19987e9
Jin Soo Ihm <jinihm@umich.edu>
2019-12-16 05:32:55 -0500

    seems to be working

--- include/isr/constraint_solver.hpp
+++ include/isr/constraint_solver.hpp
@@ -87,11 +87,9 @@ private:
             }
 
          // merge instead of sort
-         std::cout << currentOccurences.size() << std::endl;
          fb::Vector<fb::SizeT> mergedOccurences(currentOccurences.size() + occurrences.size());
 	      std::merge(currentOccurences.begin(), currentOccurences.end(), occurrences.begin(), occurrences.end(), mergedOccurences.begin());
          occurrences = std::move(mergedOccurences);
-         std::cout << occurrences.size() << std::endl;
          }
 
       // tfidf
